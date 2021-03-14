b8c391de8f74de20084bb25e7f47ee35d476dc68
TER <te0006@web.de>
2021-03-14 22:47:48 +0100

    split env

--- src/boot.py
+++ src/boot.py
@@ -1,11 +1,11 @@
-import network, ntptime, machine, wifisettings, time
+import network, ntptime, machine, wifisettings, nodesettings, time
 import lib.timew
 
 sta_if = network.WLAN(network.STA_IF)
 if not sta_if.isconnected():
   print('Connecting to network...')
   sta_if.active(True)
-  sta_if.config(dhcp_hostname=wifisettings.settings['controllerName'])
+  sta_if.config(dhcp_hostname=nodesettings.settings['controllerName'])
   sta_if.connect(wifisettings.settings['wifiAP'], wifisettings.settings['wifiPassword'])
   while not sta_if.isconnected():
     pass
