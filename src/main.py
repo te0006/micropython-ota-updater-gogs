5dde9d66a5a75f81292b88ffda1aaf6a80a155f8
TER <te0006@web.de>
2021-03-21 13:35:35 +0100

    refactoring for mqtt-focused applications

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-import update, wifisettings, nodesettings, lib.requests, lib.logger, lib.requests, lib.timew, time, os, machine
+import time, machine, update, wifisettings, nodesettings, lib.requests, lib.logger, lib.requests, lib.timew, time, os, machine
+from umqtt.robust import MQTTClient
+from machine import Pin, WDT
+led = Pin(2, Pin.OUT)
+led.off()
 
 t = lib.timew.Time(time=time)
 
@@ -6,6 +10,27 @@ t = lib.timew.Time(time=time)
 logger = lib.logger.config(enabled=nodesettings.settings['debug'], include=nodesettings.settings['logInclude'], exclude=nodesettings.settings['logExclude'], time=t)
 log = logger(append='boot')
 log("The current time is %s" % t.human())
+
+mqtt_pth = nodesettings.settings['controllerName']
+client = MQTTClient(mqtt_pth, 
+wifisettings.settings['mqtt_ip'], 
+port=wifisettings.settings['mqtt_port'])
+
+mq_c = False
+for i in range(1):
+  try:
+    client.connect()
+    mq_c = True
+  except:
+    log('MQTT: cannot connect')
+    led.on()
+    time.sleep(1)
+if not mq_c:
+  log ('MQTT: giving up, resetting')
+  time.sleep(1)
+  machine.reset()
+log('MQTT: connected OK')
+led.off()
 
 loggerOta = logger(append='OTAUpdater')
 
@@ -22,8 +47,34 @@ updater = update.OTAUpdater(io=io, gogs=gogs, logger=loggerOta, machine=machine)
 
 try:
   updater.update()
+  log('OTA update OK')
+  client.publish(mqtt_pth+"/BOOT", "OTA_OK")
+  led.off()
 except Exception as e:
   log('Failed to OTA update:', e)
+  client.publish(mqtt_pth+"/BOOT", "OTA_error")
+  led.on()
 
+
+env = {}
+env.update(nodesettings.settings)
+env['requests'] = lib.requests
+env['logger'] = logger
+env['time'] = t
+env['updater'] = updater
+
+env['wdt'] = WDT(timeout=5000) # watchdog timer. If feed() is not called every 5000ms (max) the system will reset.
+    # the main loop (within src.main.start) calls feed() periodically but will stop doing so in case of an exception
+    # If communication to the MQTT broker fails, an exception will occur.
+    # Thus, the system can be remote-reset by stopping the MQTT broker.
+
+def mqtt_pub(tag, data):
+    client.publish(mqtt_pth+"/"+tag, 
+    data)
+
+env['mqtt_pub'] = mqtt_pub
+env['wdt'].feed()
+
+log('launching src.main.start()')
 import src.main
-src.main.start(env=nodesettings, requests=lib.requests, logger=logger, time=t, updater=updater)
+src.main.start(env)
